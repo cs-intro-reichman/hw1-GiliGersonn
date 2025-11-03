b52923ba0ce6fdff7a989240e26d7976eb5f856b
Gili Gerson <gili.gerson@post.runi.ac.il>
2025-11-03 23:35:05 +0200

    test

--- NumWords.java
+++ NumWords.java
@@ -6,7 +6,7 @@ public class NumWords {
 		int num = Integer.parseInt(args[0]); 
 		int hundreds = (num / 100); 
 		int tens = ((num / 10) % 10);  
-		int ones = ((num % 100));
+		int ones = ((num % 10));
 		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
 
 
